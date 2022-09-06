168930268f6319519bb9a3ee37914936806b2d70
Betty Nguyen <bettyanhn@gmail.com>
2022-09-06 10:45:22 -0700

    Change hello world greeting to a message about my favourite animal, add javadoc, fix indentation

---
+++ src/main/java/org/comp2522/Main.java
@@ -0,0 +1,10 @@
+package org.comp2522;
+
+/**
+ * Runs the program to print a message.
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("I love shiba inus!");
+  }
+}
